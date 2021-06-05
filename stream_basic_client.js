7908e3b5a01e08de2e069036806f90e8375d9a35
Ubuntu <ubuntu@ip-172-31-89-105.ec2.internal>
2021-06-05 06:12:12 +0000

    batch and stream basic completed

--- stream_basic_client.js
+++ stream_basic_client.js
@@ -1,13 +1,17 @@
 var Gearman = require("abraxas");
 
-var studentNum = 30;
-var problemNum = 25;
-var nodeNum = 3;
-var inputPerBatch = 7;
+var batchMode = false;
+var streamMode = false;
 
+var nodeNum = 3;
 var score = [];
+
+var taskNum = 0;
+
 var sumInput = [];
 var countInput = [];
+
+var sumOutputNum = 0;
 
 var sumRRpointer = 0;
 var countRRpointer = 0;
@@ -30,17 +34,18 @@ var output = [];
 function timeout(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
-function setDividingValues() {
-  quotient = Math.floor(score.length / inputPerBatch);
-  remainder = score.length % inputPerBatch;
+function setMode() {
+  var arg = Number(process.argv[2]);
+  if (arg == 0) batchMode = true;
+  else if (arg == 1) streamMode = true;
 }
 
-function getRandomScore() {
-  return Math.floor(Math.random() * 5);
-}
+function setValues() {
+  var studentNum = 31;
+  var problemNum = 3;
+  var quotient;
+  var remainder;
 
-function initializeInput() {
   for (let i = 0; i < studentNum; i++) {
     var temp = [];
     for (let j = 0; j < problemNum; j++) {
@@ -48,6 +53,56 @@ function initializeInput() {
     }
     score.push(temp);
   }
+
+  if (batchMode) {
+    // studentNum   tasknum = nodeNum  quotient    (quotient+remainder)
+    //     29               3            9 9               (9+2)
+    //     30               3           10 10              (10+0)
+    //     31               3           10 10              (10+1)
+    quotient = Math.floor(studentNum / nodeNum); // 10
+    remainder = studentNum % nodeNum;
+    taskNum = nodeNum;
+
+    for (var i = 0; i < taskNum; i++) {
+      var input = [];
+      for (var j = 0; j < quotient; j++) {
+        input.push(score[i * quotient + j]);
+      }
+      // last loop
+      if (i == taskNum - 1) {
+        for (var j = 0; j < remainder; j++) {
+          input.push(score[quotient * nodeNum + j]);
+        }
+      }
+      sumInput.push(input);
+    }
+  } else if (streamMode) {
+    // studentNum  IPB    quotient (remainder)   tasknum
+    //     27       7     7 7 7        (6)          4
+    //     28       7     7 7 7 7                   4
+    //     29       7     7 7 7 7      (1)          5
+    var inputPerBatch = 7;
+    quotient = Math.floor(studentNum / inputPerBatch);
+    remainder = studentNum % inputPerBatch;
+    taskNum = Math.floor((studentNum - 1) / inputPerBatch) + 1;
+
+    for (var i = 0; i <= quotient; i++) {
+      var input = [];
+      for (var j = 0; j < inputPerBatch; j++) {
+        if (i == quotient && j == remainder) break;
+        input.push(score[i * inputPerBatch + j]);
+      }
+      if (input.length > 0) sumInput.push(input);
+    }
+  }
+
+  // console.log(score);
+  // console.log("----------------------------------------");
+  // for (var i = 0; i < sumInput.length; i++) console.log(sumInput[i]);
+}
+
+function getRandomScore() {
+  return Math.floor(Math.random() * 5);
 }
 
 function initializeOutput() {
@@ -56,28 +111,15 @@ function initializeOutput() {
   }
 }
 
-function makeJobIntoTask() {
-  for (var i = 0; i <= quotient; i++) {
-    var input = [];
-    for (var j = 0; j < inputPerBatch; j++) {
-      if (i == quotient && j == remainder) break;
-      input.push(score[i * inputPerBatch + j]);
-    }
-    sumInput.push(input);
-  }
-}
-
 function executeSum() {
   if (sumInput.length == 0) return;
-
   // load-balancing strategy : round-robin
   let client = Gearman.Client.connect({
     servers: ["127.0.0.1:" + (sumRRpointer + 4730)],
     defaultEncoding: "utf8",
   });
   sumRRpointer++;
-  sumRRpointer %= nodeNum;
-
+  sumRRpointer %= nodeNum; // control index
   // execute
   let input = sumInput[0];
   sumInput.splice(0, 1); // remove item on index 0
@@ -90,10 +132,12 @@ function executeSum() {
       let time = endTime - startTime;
       console.log(`------------SUM : ${time}ms elapsed------------`);
       countInput.push(sum);
+      sumOutputNum++;
     });
 }
 
 function executeCount() {
+  if (batchMode && sumOutputNum < taskNum) return;
   if (countInput.length == 0) return;
 
   // load-balancing strategy : round-robin
@@ -103,7 +147,6 @@ function executeCount() {
   });
   countRRpointer++;
   countRRpointer %= nodeNum;
-
   // execute
   let input = countInput[0];
   countInput.splice(0, 1); // remove item on index 0
@@ -115,15 +158,12 @@ function executeCount() {
       let endTime = Date.now();
       let time = endTime - startTime;
       console.log(`------------COUNT : ${time}ms elapsed------------`);
-
       /* update output */
       for (let k = 0; k <= 100; k++) {
         output[k][1] += count[k][1];
       }
-
       /* count finish */
       finish--;
-
       /* check final output */
       if (finish == 0) {
         console.log("---------------FINAL OUTPUT---------------");
@@ -133,31 +173,20 @@ function executeCount() {
 }
 
 async function executeBatch() {
-  finish = Math.floor((score.length - 1) / inputPerBatch) + 1;
+  finish = taskNum;
   // all jobs are completed if finish is 0
-
   while (finish != 0) {
     executeSum();
     executeCount();
     await timeout(1); // prevent busy waiting (make idle for callback function)
   }
-
   console.log("PROGRAM EXIT");
 }
 
-initializeInput();
-// console.log(score);
+setMode();
 
-setDividingValues();
-// console.log(quotient);
-// console.log(remainder);
-
-makeJobIntoTask();
-// for (var i = 0; i < sumInput.length; i++) {
-//   console.log(sumInput[i]);
-// }
+setValues();
 
 initializeOutput();
-// console.log(output);
 
 executeBatch();
